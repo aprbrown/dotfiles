24b2d53f69d75c1495256dba3b5087a87b9070e0
Andrew Brown <hello@andrewprbrown.com>
2022-10-02 14:07:48 +0100

    Initial commit

---
+++ dot_config/nvim/lua/user/set.lua
@@ -0,0 +1,35 @@
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.cmdheight = 2
+vim.opt.conceallevel = 0
+vim.opt.fileencoding = "utf-8"
+vim.opt.pumheight = 10
+vim.opt.showtabline = 2
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.updatetime = 300
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 16
+vim.opt.sidescrolloff = 16
+vim.opt.showmode = true
+
+vim.g.mapleader = " "
